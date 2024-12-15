430d59662c7e761e70def05327f59ccdac15feed
ROMUSKING <romusking@gmail.com>
2024-12-15 19:26:42 +0000

    amended textnode repr

--- src/textnode.py
+++ src/textnode.py
@@ -26,7 +26,7 @@ class TextNode:
             self.url == other.url)
         
     def __repr__(self):
-        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+        return f"TextNode({self.text}, {self.text_type.value[0]}, {self.url})"
 
 
 def text_node_to_html_node(text_node):
